412b5a5fdd46011be9391ea40327b9bab3140eb6
houston1337 <forspam-8@yandex.ru>
2024-02-04 22:01:00 +0300

    commit 1/1 for day 035

---
+++ 1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Укажите путь к файлу
+file_path="my_git.txt"
+
+# Укажите ключ, который вы ищете
+desired_key=$(date +"%j")
+
+# Поиск значения по ключу
+value=$(grep "^$desired_key=" "$file_path" | cut -d '=' -f2)
+
+# Проверка наличия значения
+if [ -n "$value" ]; then
+    echo "Значение для ключа '$desired_key': $value"
+else
+    echo "Ключ '$desired_key' не найден в файле."
+fi
+if [ $value != 0 ]; then
+	for i in $(seq 1 $value)
+	do
+		echo -e "day $desired_key \ncommit $i/$value">1.txt
+		git add 1.txt
+		git commit -m "commit $i/$value for day $desired_key"
+	done
+	git push -u origin main
+else
+	echo "no commits planned for today"
+fi
